4c1ec1483da4d4489bcaaa21b7065b085c0d31d2
jfc.me/ <fernandes_punk_@hotmail.com>
2020-03-18 23:25:34 -0400

    Update COVID-19 CORONAVIRUS OUTBREAK.py

--- COVID-19 CORONAVIRUS OUTBREAK.py
+++ COVID-19 CORONAVIRUS OUTBREAK.py
@@ -7,7 +7,7 @@ def coronavirus():
     dic = json.loads(requests.get("https://exchange.vcoud.com/coronavirus/latest").text)
     outputFile = open('COVID-19 CORONAVIRUS OUTBREAK.csv',	'w',	newline='')
     outputWriter = csv.writer(outputFile)
-    outputWriter.writerow(['Date', 'Pais', 'TotalDeaths', 'SeriousCases','TotalCases'])
+    outputWriter.writerow(['Date', 'countrie', 'TotalDeaths', 'SeriousCases','TotalCases'])
     print("countrie   Total Cases")
     for elem in dic:
         print("%s    %s "%(elem['name'],elem['totalCases']))
