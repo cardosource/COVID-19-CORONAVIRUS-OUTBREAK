311f6b1c16dcf1647601ee298f37d8e9a8168e28
jeferson cardoso <jefersoncardoso.dev@gmail.com>
2021-06-29 02:52:55 -0400

    update script
    Melhorando a requisição

--- COVID-19 CORONAVIRUS OUTBREAK.py
+++ COVID-19 CORONAVIRUS OUTBREAK.py
@@ -1,17 +1,38 @@
+import threading
+import time
+from queue import Queue
 import requests
 import json
 import csv
-import threading
-
-def coronavirus():
-    dic = json.loads(requests.get("https://exchange.vcoud.com/coronavirus/latest").text)
+def solicitacao(url):
+  
+    resp = json.loads(requests.get(url).text)
     outputFile = open('COVID-19 CORONAVIRUS OUTBREAK.csv',	'w',	newline='')
     outputWriter = csv.writer(outputFile)
     outputWriter.writerow(['Date', 'countrie', 'TotalDeaths', 'SeriousCases','TotalCases'])
     print("countrie   Total Cases")
-    for elem in dic:
-        print("%s    %s "%(elem['name'],elem['totalCases']))
-        outputWriter.writerow([elem['updatedAt'],elem['name'],elem['totalCases'],elem['totalDeaths'],elem['seriousCases']])
-    outputFile.close()
-threadObj = threading.Thread(target=coronavirus)
-threadObj.start()
+    with conteudo:
+        for elem in resp:
+            print("%s    %s "%(elem['name'],elem['totalCases']))
+            outputWriter.writerow([elem['updatedAt'],elem['name'],elem['totalCases'],elem['totalDeaths'],elem['seriousCases']])
+        outputFile.close()
+
+def gerenciador():
+
+    while True:
+        url_presente = fila.get()
+        solicitacao(url_presente)
+        fila.task_done()
+
+quantidade_threads = 5
+conteudo = threading.Lock()
+fila = Queue()
+url = "https://exchange.vcoud.com/coronavirus/latest"
+for i in range(quantidade_threads):
+     t = threading.Thread(target=gerenciador)
+     t.daemon = True
+     t.start()
+     start = time.time()
+     fila.put(url)
+fila.join()
+print((time.time() - start))
